013bf04811d6a82c59799ed9f7b67444f5da074d
Akruti <akrutip28@gmail.com>
2020-09-24 10:08:49 +0530

    Modified horizontal piece

--- chessBoard/main.py
+++ chessBoard/main.py
@@ -1,51 +1,29 @@
 
 class Grid:
 
-    numToAlpha = {
-        1: "a",
-        2: "b",
-        3: "c",
-        4: "d",
-        5: "e",
-        6: "f",
-        7: "g",
-        8: "h"
-    }
+    def __init__(self,width,height):
+        self.w = width
+        self.h = height
 
-    alphaToNum = {
-        "a": 1,
-        "b": 2,
-        "c": 3,
-        "d": 4,
-        "e": 5,
-        "f": 6,
-        "g": 7,
-        "h": 8
-    }
+    def create_grid(self,w,h):
 
-    w = int(input("how wide: "))
-    h = int(input("how tall: "))
+        columns = []
+        rows = []
 
-    columns = []
-    rows = []
+        iterations = 1
+        while(iterations <= w):
+            columns.append(iterations)
+            iterations += 1
 
-    iterations = 1
-    while(iterations <= w):
-        columns.append(iterations)
-        iterations += 1
+        iterations = 1
+        while(iterations <= h):
+            rows.append(chr(iterations + 64))
+            iterations += 1
 
-    iterations = 1
-    while(iterations <= h):
-        rows.append(numToAlpha[iterations])
-        iterations += 1
-
-    for alpha in rows:
-        for num in columns:
-            print(alpha,num,end = "   ")
-        print("\n")
-
-
-grid = Grid()
+        for alpha in rows:
+            for num in columns:
+                print(alpha,num,end = "   ")
+            print("\n")
 
 class Piece:
 
@@ -61,19 +39,39 @@ class HorizontalMovablePiece(Piece):
     def __init__(self, positionRow, positionColumn):   # allowed_spaces, can_move_across_board):
         super().__init__(positionRow, positionColumn)  # allowed_spaces, can_move_across_board)
 
+    def move(self,w,h):
 
-    def move(self):
         iterations = 1
-        while(iterations <= grid.w):
+        while(iterations <=w):
             if(iterations != self.positionColumn):
-                print(grid.numToAlpha[self.positionRow],iterations)
+                print(chr(self.positionRow +64),iterations, '',end='')
             iterations+=1
 
 
-row_input = int(input('row position: '))
-col_input = int(input('col position: '))
+    # def move(self, allowed_spaces,w):
+    #
+    #     iterations = 1
+    #     while(iterations<=w):
+    #         if(iterations != self.positionColumn):
+    #             print(chr(self.positionRow+64),iterations)
+    #         iterations+=1
 
-horizontalMovableObject = HorizontalMovablePiece(row_input,col_input)
-horizontalMovableObject.move()
+
+class King():
+    pass
+
+def main():
+
+    w = int(input("how wide: "))
+    h = int(input("how tall: "))
+    grid = Grid(w,h)
+    grid.create_grid(w,h)
+
+    row_input = int(input('row position: '))
+    col_input = int(input('col position: '))
+    horizontalMovableObject = HorizontalMovablePiece(row_input, col_input)
+    horizontalMovableObject.move(2,w)
 
 
+if __name__ == "__main__":
+        main()
