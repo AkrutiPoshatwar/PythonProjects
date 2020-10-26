10d1c34979acf5dabc1453ad147de1030452dc31
Akruti <akrutip28@gmail.com>
2020-10-26 16:19:14 +0530

    third exercise

--- Excercise_4/second.py
+++ Excercise_4/second.py
@@ -1,9 +1,13 @@
 import json
 
-with open('jsonfile.json','r') as f:
+with open('data_file.json') as f:
     data = json.load(f)
 
 count = {}
 for i in data:
     count[i] = count.get(i, 0) + 1
 print(count)
+
+
+with open('frequency.json', 'w') as json_file:
+  json.dump(count, json_file)
