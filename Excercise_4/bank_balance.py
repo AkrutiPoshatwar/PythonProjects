bc5dbc83088b5222493b2a2e001b4a789d39e923
Akruti <akrutip28@gmail.com>
2020-10-27 13:47:13 +0530

    using in operator

--- Excercise_4/bank_balance.py
+++ Excercise_4/bank_balance.py
@@ -2,25 +2,18 @@ import json
 
 with open('transaction_log.json', 'r') as f:
     data = json.load(f)
-bank_balance = {}
 
-balance = 0
+users_dict = {}
+for i in range(len(data['users'])):
+    users_dict.update({data['users'][i]['account_id']: data['users'][i]['current_balance']})
 
-for j in range(len(data['bank_transactions'])):
 
-    for i in range(len(data['users'])):
+for transaction in data['bank_transactions']:
+    if transaction['account_id'] in users_dict:
 
-        if data['users'][i]['acc_id'] == data['bank_transactions'][j]['acc_id']:
-            balance = data['users'][i]['current_balance']
+        if transaction['type'] == 'credit':
+            users_dict[transaction['account_id']] += transaction['amount']
+        else:
+            users_dict[transaction['account_id']] -= transaction['amount']
 
-            if data['bank_transactions'][j]['type'] == 'credit':
-                balance = balance + data['bank_transactions'][j]['amount']
-                data['users'][i]['current_balance'] = balance
-            else:
-                balance = balance - data['bank_transactions'][j]['amount']
-                data['users'][i]['current_balance'] = balance
-
-            bank_balance.update({data['users'][i]['acc_id']: balance})
-
-print(bank_balance)
-
+print(users_dict)
