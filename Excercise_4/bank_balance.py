987d273829eb11aa103fa9850b571dd366ec25d0
Akruti <akrutip28@gmail.com>
2020-10-27 11:05:30 +0530

    Correct bank balance

--- Excercise_4/bank_balance.py
+++ Excercise_4/bank_balance.py
@@ -2,20 +2,24 @@ import json
 
 with open('transaction_log.json', 'r') as f:
     data = json.load(f)
-
 bank_balance = {}
 
 balance = 0
+
 for j in range(len(data['bank_transactions'])):
 
     for i in range(len(data['users'])):
 
         if data['users'][i]['acc_id'] == data['bank_transactions'][j]['acc_id']:
+            balance = data['users'][i]['current_balance']
+
             if data['bank_transactions'][j]['type'] == 'credit':
-                balance = data['users'][i]['current_balance'] + data['bank_transactions'][j]['amount']
+                balance = balance + data['bank_transactions'][j]['amount']
+                data['users'][i]['current_balance'] = balance
             else:
-                balance = data['users'][i]['current_balance'] - data['bank_transactions'][j]['amount']
-            print(balance)
+                balance = balance - data['bank_transactions'][j]['amount']
+                data['users'][i]['current_balance'] = balance
+
             bank_balance.update({data['users'][i]['acc_id']: balance})
 
 print(bank_balance)
