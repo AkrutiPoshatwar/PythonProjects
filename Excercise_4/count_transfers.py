9de828723421cb55e13fd7f0949def8e99960b98
Akruti <akrutip28@gmail.com>
2020-10-30 10:11:44 +0530

    using user_dict to print

--- Excercise_4/count_transfers.py
+++ Excercise_4/count_transfers.py
@@ -6,45 +6,25 @@ with open('users_transfers_data.json') as f:
 sorted_bank_transfers = sorted(data['bank_transfers'],key=lambda x : x['date'], reverse=False)
 
 users_dict = {}
-for user in data['users']:
-    users_dict[user['account_id']] = user['current_balance']
 
-invalid_transfers ={}
-credited_to = {}
-debited_to ={}
+for user in data['users']:
+    users_dict[user['account_id']] = {
+                                      'credit':0,
+                                      'debit':0,
+                                      'invalid_transfers':[],
+                                      'current_balance':user['current_balance']}
 
 for transfer in sorted_bank_transfers:
 
-    if transfer['amount'] <= users_dict[transfer['from_account_id']]:
-        users_dict[transfer['from_account_id']] -= transfer['amount']
-        users_dict[transfer['to_account_id']] += transfer['amount']
+    if transfer['amount'] <= users_dict[transfer['from_account_id']]['current_balance'] :
+        users_dict[transfer['from_account_id']]['current_balance'] -= transfer['amount']
+        users_dict[transfer['to_account_id']]['current_balance'] += transfer['amount']
 
-
-        if transfer['from_account_id'] in debited_to:
-            debited_to[transfer['from_account_id']]+= 1
-        else:
-            debited_to.update({transfer['from_account_id']:1})
-
-        if transfer['to_account_id'] in credited_to:
-            credited_to[transfer['to_account_id']] += 1
-        else:
-            credited_to.update({transfer['to_account_id']: 1})
+        users_dict[transfer['from_account_id']]['debit'] += 1
+        users_dict[transfer['to_account_id']]['credit'] += 1
 
     else:
-        if transfer['from_account_id'] in invalid_transfers:
-            invalid_transfers[transfer['from_account_id']].append(transfer['date'])
-        else:
-            invalid_transfers[transfer['from_account_id']] = [transfer['date']]
+        users_dict[transfer['from_account_id']]['invalid_transfers'].append(transfer['date'])
 
-print(invalid_transfers)
-
-transfer_info={}
-for key in users_dict:
-    transfer_info['credits'] = credited_to[key]
-    transfer_info['debits'] = debited_to[key]
-    transfer_info['Invalid_transfers'] = invalid_transfers[key]
-    transfer_info['current_balance'] = users_dict[key]
-
-    users_dict[key] = transfer_info
 
 print(json.dumps(users_dict, indent=4))
