614cf98dd8e81fd0e25b9b13293a54959535036b
Akruti <akrutip28@gmail.com>
2020-11-02 16:11:19 +0530

    Adding main function

--- ProductSale/total_price.py
+++ ProductSale/total_price.py
@@ -1,32 +1,36 @@
 import json
 
+def main(path ='product_data.json'):
 
-with open('product_data.json') as f:
-    data= json.load(f)
+    with open(path) as f:
+        data = json.load(f)
 
-sorted_products = sorted(data,key=lambda x : x['totalSales'], reverse=True)
+    sorted_products = sorted(data, key=lambda x: x['totalSales'], reverse=True)
 
-products_price =0
-cumulative_percentage=0
-products =[]
-currency_dict ={'euro':86 , 'doller':76, 'rupee':1 }
+    total_price = 0
+    cumulative_percentage = 0
+    products_list = []
+    currency_dict = {'euro': 86, 'doller': 76, 'rupee': 1}
+
+    for product in sorted_products:
+
+        if product['currency'] != 'rupee':
+            product['price'] = product['price'] * currency_dict[product['currency']]
+        total_price += product['totalSales']* product['price']
+
+    for product in sorted_products:
+        
+        percentage = (product['totalSales']*product['price']/total_price)*100
+        cumulative_percentage +=percentage
+
+        product_dict ={'name':product['name'],
+                       'totalSales': product['totalSales'],
+                       'percentage':str(int(percentage))+'%',
+                       'cumulativePercentage':str(int(cumulative_percentage))+'%'}
+
+        products_list.append(product_dict)
+    print(json.dumps(products_list, indent =4))
 
 
-for product in sorted_products:
-    if product['currency'] != 'rupee':
-        product['price'] = product['price'] * currency_dict[product['currency']]
-    products_price += product['totalSales']* product['price']
-
-print(products_price)
-
-for product in sorted_products:
-    product_percentage = (product['totalSales']*product['price']/products_price)*100
-    cumulative_percentage +=product_percentage
-
-    product_dict ={'name':product['name'],
-                   'totalSales': product['totalSales'],
-                   'percentage':str(int(product_percentage))+'%',
-                   'cumulativePercentage':str(int(cumulative_percentage))+'%'}
-
-    products.append(product_dict)
-print(json.dumps(products, indent =4))
+if __name__ == "__main__":
+    main()
